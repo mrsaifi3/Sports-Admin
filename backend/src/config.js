57a2ea23cbda2d71d3ce4291d7099c1b14519e42
mrsaifi3 <srock3606@gmail.com>
2024-12-09 13:51:02 +0530

    Update config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,21 +3,21 @@ export const templateImageConfig = [
     {
         filename: "athelete-front.png",
         localPath: "./",
-        downloadUrl: process.env.ATHELETE_FRONT_URL,
+        downloadUrl: "https://res.cloudinary.com/dwiouayh7/image/upload/v1729399293/3_okvmcp.png", 
     },
     {
         filename: "athelete-back.png",
         localPath: "./",
-        downloadUrl: process.env.ATHELETE_BACK_URL,
+        downloadUrl: "https://res.cloudinary.com/dwiouayh7/image/upload/v1729399293/4_kifunp.png",
     },
     {
         filename: "coach-front.png",
         localPath: "./",
-        downloadUrl: process.env.COACH_FRONT_URL,
+        downloadUrl: "https://res.cloudinary.com/dwiouayh7/image/upload/v1729399292/1_wv2hk4.png",
     },
     {
         filename: "coach-back.png",
         localPath: "./",
-        downloadUrl: process.env.COACH_BACK_URL,
+        downloadUrl: "https://res.cloudinary.com/dwiouayh7/image/upload/v1729399292/2_k22qc4.png",
     },
 ];
